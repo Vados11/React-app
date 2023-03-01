5d2cad9bbe4b11fff00ddfdece0bef3b49a7feb7
Vados11 <vlad.davydenko.2002@gmail.com>
2023-03-01 17:36:41 +0200

    content html

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://marketplace.canva.com/EAEz7zOew58/2/0/1600w/canva-moon-light-logo-template-8pEo1H1XNj4.jpg"
+                alt="img"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='content-image'>
+            </div>
+            <div>
+                Ava + descriptions
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
